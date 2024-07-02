b9cdb80eb1eff621c60d43f2708e1753f006def7
Fabian Vazquez <fabo.rp@gmail.com>
2024-07-01 21:26:50 -0300

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>Desarrollado por Fabian Vazquez.</h2>
+        </footer>
+    )
+}
+
+export default Footer;
